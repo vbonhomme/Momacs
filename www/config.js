54bda6fd7d756a93f70100da1ddd23153f22eb3e
jlopez <jimmy.lopez0023@gmail.com>
2018-03-22 17:33:36 +0100

    Add gestion partition to UI and clean code

--- www/config.js
+++ www/config.js
@@ -3,9 +3,11 @@ var momacs_config = {
   color_segment: "#00b894",
   color_curve: "#0984e3",
   color_hover: "#8e44ad",
+  color_text: "#2c3e50",
   size_point: 3,
   size_hover: 3,
   size_segment: 1.5,
-  size_curve: 1.5
+  size_curve: 1.5,
+  size_text: 8
   
 };
